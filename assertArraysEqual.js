dd059fc42f33c98258d8c4198e30f7070c792d81
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2022-11-19 21:43:22 -0600

    add solution to challenge

---
+++ assertArraysEqual.js
@@ -0,0 +1,28 @@
+const assertArraysEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😍😍😍Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😭😭😭Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+// TEST CODE
+
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
