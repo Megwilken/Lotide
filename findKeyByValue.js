79e5071e6c2661b0aad0ec89a6b7e71441fbbcd7
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2022-11-23 09:56:31 -0600

    added initial object and function with comments on how to solve

---
+++ findKeyByValue.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😍😍😍Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😭😭😭Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+
+// implement a function that scans the object (object = bestTVShowsByGenre), and returns the first 
+// key (key = genres) that contain's the given value (value = show names)
+
+//Object
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+// Implement find key by value function
+const findKeyByValue = function(object, value) {
+// Object.keys function to search through the object keys
+const keys = Object.keys(findKeyByValue);
+//for of statement that loops over what was returned by Object.keys
+
+// if statement?
+
+// return statement? 
+
+ 
+
+// TEST CODE
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
