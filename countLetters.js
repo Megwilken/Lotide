e99ef43e230feba05f9b442814478615c141b880
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2022-11-22 21:49:48 -0600

    implement remaining intial code

--- countLetters.js
+++ countLetters.js
@@ -9,14 +9,22 @@ const assertEqual = function(actual, expected) {
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 
-// declare countLetters function
-const countLetters = function(string){
-//define empty variable
-const result = {};
-//for... of loop
-for (const letters of string) {
-  console.log(letters);
-}
-}
+// declare countLetters function that takes in a string
+const countLetters = function(string) {
+//define empty variable to store result
+  const result = {};
+  //for... of loop to loop over characters of string
+  for (let letters of string) {
+    //increment counter - if letter exists add it to tally
+    if (result[letters]) {
+      result[letters] += 1;
+    } else {
+      result[letters] = 1;
+    }
+  }
+  return result;
+};
 //TEST CODE
 console.log(countLetters("LHL"));
+console.log(countLetters("Hi. Hello. How Are You?"));
+console.log(countLetters("Lighthouse Labs"));
