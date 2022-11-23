5bde7a92316abaf81b453479ceaeea4413a1392e
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2022-11-23 10:37:55 -0600

    add additional code - still need to debug

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -10,9 +10,26 @@ assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 
 
-// implement a function that scans the object (object = bestTVShowsByGenre), and returns the first 
+// implement a function that scans the object (object = bestTVShowsByGenre), and returns the first
 // key (key = genres) that contain's the given value (value = show names)
 
+
+
+// Implement find key by value function
+const findKeyByValue = function(object, value) {
+
+  // Object.keys function to search through the object keys
+  const key = Object.keys(bestTVShowsByGenre);
+
+  //for of statement that loops over what was returned by Object.keys
+  for (let genres of key) 
+    // if statement?
+    if (object[key] === value) {
+
+      // return statement?
+      return key;
+    }
+  }
 //Object
 const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
@@ -20,18 +37,8 @@ const bestTVShowsByGenre = {
   drama: "The Wire"
 };
 
-// Implement find key by value function
-const findKeyByValue = function(object, value) {
-// Object.keys function to search through the object keys
-const keys = Object.keys(findKeyByValue);
-//for of statement that loops over what was returned by Object.keys
-
-// if statement?
-
-// return statement? 
-
- 
-
 // TEST CODE
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+
