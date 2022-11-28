35767f5874e4603e8f781806e6dd66743eac07e1
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2022-11-27 21:02:42 -0600

    further debugging on code complete, results correct but assertion failing

--- takeUntil.js
+++ takeUntil.js
@@ -2,17 +2,17 @@
 Implement takeUntil which will keep collecting items from a provided array until the callback provided returns a truthy value.
 
 */
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+// const eqArrays = function(arrayOne, arrayTwo) {
+//   if (arrayOne.length !== arrayTwo.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arrayOne.length; i++) {
+//     if (arrayOne[i] !== arrayTwo[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
 
 const assertArraysEqual = function(actual, expected) {
@@ -24,25 +24,27 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 //Implement takeUntil which will keep collecting items from a provided array until the callback provided returns a truthy value.
-const takeUntil = (array, callback) => {
+const takeUntil = function(array, callback) {
   let results = [];
-  for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    if (callback(element)) {
+  for (let items in array) {
+    if (callback(array[items])) {
+      console.log(array);
+      results = array.slice(0, items);
+      break;
     }
-  }  
-    return results;
-}
-
+  }
+  return results;
+};
+      
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);                            // [ 1, 2, 5, 7, 2 ]
-assertArraysEqual(eqArrays(results1, [1, 2, 5, 7, 2 ]));
+assertArraysEqual(results1, [1, 2, 5, 7, 2 ]);
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ",");
 console.log(results2);                              //[ 'I\'ve', 'been', 'to', 'Hollywood' ]
-assertArraysEqual(eqArrays(results2, ["I've", "been", "to", "Hollywood"]));
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
 
