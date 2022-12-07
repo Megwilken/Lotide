3f94cfe2bd13f88845b01a6b77b21b8caa6be0e0
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2022-12-06 23:18:46 -0600

    refactored lotide

--- assertEqual.js
+++ assertEqual.js
@@ -4,10 +4,8 @@ const assertEqual = function(actual, expected) {
   } else if (actual !== expected) {
     console.log(`😭😭😭Assertion Failed: ${actual} !== ${expected}`);
   }
-};
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+}
+module.exports = assertEqual;
 
 
 
