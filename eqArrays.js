606d6191fbac6050d1980ead86eab8df6b41d3e2
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2022-12-06 23:30:12 -0600

    refactoring lotide

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😍😍😍Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`😭😭😭Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arrayOne, arrayTwo) {
   if (arrayOne.length !== arrayTwo.length) {
     return false;
@@ -17,13 +9,5 @@ const eqArrays = function(arrayOne, arrayTwo) {
   }
   return true;
 };
-// TEST CODE
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]));
-
-
+module.exports = eqArrays;
