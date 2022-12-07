7da7d2ec3efc466094ad37b50576705427de35b8
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2022-12-06 23:42:04 -0600

    refactoring Lotide

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-const assertArraysEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😍😍😍Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`😭😭😭Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// declare function
-// if function is even number, return empty array []
-// if function is odd number, return middle elements
-
 const middle = function(array) {
   let middleArray = Math.floor((array.length / 2));
   if (array.length <= 2) {
@@ -31,19 +8,5 @@ const middle = function(array) {
     return [array[Math.floor(array.length / 2)]];
   }
 };
-// TEST CODE
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = middle;
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,20 @@
 const eqArrays = require('../eqArrays.js');
+const middle = require('../middle.js');
 const assertArraysEqual = require('../assertArraysEqual.js');
 
 // TEST CODE
+console.log(middle([1])); 
+console.log(middle([1, 2])); 
+
+console.log(middle([1, 2, 3])); 
+console.log(middle([1, 2, 3, 4, 5])); 
+
+console.log(middle([1, 2, 3, 4])); 
+console.log(middle([1, 2, 3, 4, 5, 6])); 
+
 assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
 assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
 assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); 
+
 assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
 assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-
